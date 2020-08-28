f6c65a0bf82472b6df66dbbfd9c950eb963f585f
minhkl <minh.luong@8bitrockr.com>
2020-08-28 23:18:34 +0700

    feat: apply react-hook-form & optional chaining

--- src/modules/Login/components/LoginForm/index.js
+++ src/modules/Login/components/LoginForm/index.js
@@ -7,25 +7,58 @@ import Checkbox from '@material-ui/core/Checkbox';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Typography from '@material-ui/core/Typography';
 import Paper from '@material-ui/core/Paper';
+import {useForm, Controller} from 'react-hook-form';
 import classes from './classes.module.css';
 
+const defaultValues = {
+  username: '',
+  password: '',
+};
+
 const LoginForm = ({className}) => {
+  const {handleSubmit, errors, control} = useForm({defaultValues});
+  const onSubmit = (data) => alert(JSON.stringify(data));
   return (
-    <Paper elevation={1} className={cs(classes.LoginForm, className)}>
-      <Typography variant="h4" component="h1">Just Speak</Typography>
-      <TextField label="Username" required variant="outlined" margin="normal" fullWidth/>
-      <TextField label="Password" required variant="outlined" margin="normal" type="password" fullWidth/>
-      <FormControlLabel
-        value="top"
-        control={<Checkbox color="primary" />}
-        label="Remember Me"
-        labelPlacement="end"
-      />
-      <Button
-        variant="contained" color="primary"
-        size="large"
-        className={classes.LoginForm_SubmitButton}
-        fullWidth>Login</Button>
+    <Paper elevation={3} className={cs(classes.LoginForm, className)}>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <Typography variant="h4" component="h1">Just Speak</Typography>
+        <Controller
+          control={control}
+          name="username"
+          rules={{required: {value: true, message: 'Please enter username'}}}
+          as={<TextField
+            error={!!errors?.username}
+            helperText={errors?.username?.message}
+            label="Username"
+            variant="outlined"
+            margin="normal"
+            fullWidth/>}
+        />
+        <Controller
+          control={control}
+          name="password"
+          rules={{required: {value: true, message: 'Please enter password'}}}
+          as={<TextField
+            label="Password"
+            error={!!errors?.password}
+            helperText={errors?.password?.message}
+            variant="outlined"
+            margin="normal"
+            type="password"
+            fullWidth/>}
+        />
+        <FormControlLabel
+          control={<Checkbox color="primary" />}
+          label="Remember Me"
+          labelPlacement="end"
+        />
+        <Button
+          variant="contained"color="primary"
+          size="large"
+          className={classes.LoginForm_SubmitButton}
+          type="submit"
+          fullWidth>Login</Button>
+      </form>
     </Paper>
   );
 };
